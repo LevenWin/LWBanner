dacfcc7f26b62b479cc0a41e481b5b80a805c55b
leven <wen@borderxlab.com>
2018-05-03 14:55:10 +0800

    Initial Commit

---
+++ Banner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Banner
+//
+//  Created by leven on 2018/5/3.
+//  Copyright © 2018年 leven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Banner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Banner
+//
+//  Created by leven on 2018/5/3.
+//  Copyright © 2018年 leven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
